64d4042dafce37a2b0d61cac5c5384c515446aa6
Igor Butuzov <ibutuzov87@gmail.com>
2022-12-02 11:56:28 +0200

    створено розмітку
    1. додано evt.preventDefault();
    2. організовано делегування;
    3. додано клік саме по зображенню.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryItem = document.querySelector('.gallery__item')
+
+const makePictures = picture => {
+    const {preview, original, description} = picture
+    return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+            <img class="gallery__image" 
+            src= '${preview}'
+            data-source='${original}' 
+            alt="'${description}'"/>
+        </a>
+    </div>`
+};
+
+const makeGallery = galleryItems.map(makePictures)
+                                .join('');
+
+gallery.insertAdjacentHTML('beforeend', makeGallery)
+
+gallery.addEventListener('click', onPictureClick);
+
+function onPictureClick(evt) {
+    evt.preventDefault();
+    const galleryEl = evt.target.classList.contains('gallery__image');
+    if (!galleryEl) {
+        return
+    }
+    let image = evt.target;
+    console.log(image.dataset.source);
+}   
