2d73904293b63420b886e49163c1a44100dae97e
Igor Butuzov <ibutuzov87@gmail.com>
2022-12-05 15:13:09 +0200

    виконано завдання-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,52 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', onPictureClick);
+
+
+const makePictures = picture => {
+    const {preview, original, description} = picture
+    return `
+    <li>
+        <a class="gallery__item" href='${original}'>
+        <img class="gallery__image"
+        src='${preview}'
+        alt="'${description}'" />
+        </a>
+    </li>`
+};
+
+const makeGallery = galleryItems.map(makePictures)
+                                .join('');
+
+gallery.insertAdjacentHTML('beforeend', makeGallery);
+
+let currentModalPicture;
+let currentModalDescription;
+
+function onPictureClick(evt) {
+    evt.preventDefault();
+
+    const galleryEl = evt.target.classList.contains('gallery__image');
+
+    if (!galleryEl) {
+        return
+    }
+    
+    currentModalPicture = evt.target.src;
+    currentModalDescription = evt.target.alt;
+    
+    console.log(currentModalPicture);
+    console.log(currentModalDescription);
+
+}   
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+});
+
